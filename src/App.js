eae32c9ca8d465c65973cc07ecadd31f16a65125
임수현 <soohyun.lim2@cj.net>
2022-03-09 23:52:08 +0900

    React & material-ui setting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained" color="primary">Hello World</Button>
       </header>
     </div>
   );
